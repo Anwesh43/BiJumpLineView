664f06b3603d85cca100a6852e932b1152e69210
Anwesh43 <anweshthecool0@gmail.com>
2019-11-29 11:24:23 +0530

    adding constants and libraries

--- bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
+++ bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bijumplineview
 /**
  * Created by anweshmishra on 29/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
