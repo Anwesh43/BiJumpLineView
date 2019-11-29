ebcf88f45b97c9e7c942778cfc479340daf55cd9
Anwesh43 <anweshthecool0@gmail.com>
2019-11-29 11:58:09 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbijumplineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbijumplineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbijumplineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bijumplineview.BiJumpLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiJumpLineView.create(this)
     }
 }
