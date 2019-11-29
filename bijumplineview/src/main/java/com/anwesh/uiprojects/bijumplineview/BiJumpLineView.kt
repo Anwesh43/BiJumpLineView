b71a121e8e754236ad3c9d068ecbba493f83c566
Anwesh43 <anweshthecool0@gmail.com>
2019-11-29 11:51:33 +0530

    adding BiJumpLine class

--- bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
+++ bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
@@ -159,4 +159,28 @@ class BiJumpLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiJumpLine(var i : Int) {
+
+        private val root : BJLNode = BJLNode(0)
+        private var curr : BJLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
