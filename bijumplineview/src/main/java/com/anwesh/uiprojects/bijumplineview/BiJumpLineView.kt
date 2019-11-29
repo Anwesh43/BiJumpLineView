d048a4607551545e556dface3c84e20bd8c5c12c
Anwesh43 <anweshthecool0@gmail.com>
2019-11-29 11:31:41 +0530

    adding BiJumpLineView class

--- bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
+++ bijumplineview/src/main/java/com/anwesh/uiprojects/bijumplineview/BiJumpLineView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawBJLNode(i : Int, scale : Float, paint : Paint) {
     drawBiLine(scale, size, paint)
     restore()
 }
+
+class BiJumpLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
